1604617f7d01072b947890cb97caf5ca542e33a1
RicardoAn92 <127223869+RicardoAn92@users.noreply.github.com>
2023-05-20 22:30:51 -0500

    JavaScript.js

--- Video25_Ejercicio11/JavaScript.js
+++ Video25_Ejercicio11/JavaScript.js
@@ -1,24 +1,30 @@
+const prompt = require("prompt-sync")();
+
 const truncar = (numero) => Math.trunc(numero);
 
-let a, b, c;
-let tiempoA, tiempoB, tiempoC, total;
-let horas, minutos;
+function calcularTiempo() {
+  let a, b, c;
+  let tiempoA, tiempoB, tiempoC, total;
+  let horas, minutos;
 
-console.log("Cantidad de A: ");
-a = parseInt(prompt());
+  console.log("Cantidad de A: ");
+  a = parseInt(prompt());
 
-console.log("Cantidad de B: ");
-b = parseInt(prompt());
+  console.log("Cantidad de B: ");
+  b = parseInt(prompt());
 
-console.log("Cantidad de C: ");
-c = parseInt(prompt());
+  console.log("Cantidad de C: ");
+  c = parseInt(prompt());
 
-tiempoA = 5 * a;
-tiempoB = 8 * b;
-tiempoC = 6 * c;
-total = tiempoA + tiempoB + tiempoC;
+  tiempoA = 5 * a;
+  tiempoB = 8 * b;
+  tiempoC = 6 * c;
+  total = tiempoA + tiempoB + tiempoC;
 
-horas = truncar(total / 60);
-minutos = total % 60;
+  horas = truncar(total / 60);
+  minutos = total % 60;
 
-console.log("Se tardará " + horas + " hora(s) y " + minutos + " minuto(s)");
+  console.log("Se tardará " + horas + " hora(s) y " + minutos + " minuto(s)");
+}
+
+calcularTiempo();
