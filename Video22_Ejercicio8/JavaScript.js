07bb65e2f56ed00ebcb2bbbc964bd109da799f5f
RicardoAn92 <127223869+RicardoAn92@users.noreply.github.com>
2023-05-20 22:24:14 -0500

    JavaScript.js

--- Video22_Ejercicio8/JavaScript.js
+++ Video22_Ejercicio8/JavaScript.js
@@ -1,19 +1,23 @@
 const leer = require("prompt-sync")();
 
-let horas, minutos, seg;
-let horas_seg, minutos_seg, total_seg;
+function calcularSegundosEquivalentes() {
+  let horas, minutos, seg;
+  let horas_seg, minutos_seg, total_seg;
 
-console.log("Ingrese horas: ");
-horas = parseInt(leer());
+  console.log("Ingrese horas: ");
+  horas = parseInt(leer());
 
-console.log("Ingrese minutos: ");
-minutos = parseInt(leer());
+  console.log("Ingrese minutos: ");
+  minutos = parseInt(leer());
 
-console.log("Ingrese segundos: ");
-seg = parseInt(leer());
+  console.log("Ingrese segundos: ");
+  seg = parseInt(leer());
 
-horas_seg = 3600 * horas;
-minutos_seg = 60 * minutos;
-total_seg = horas_seg + minutos_seg + seg;
+  horas_seg = 3600 * horas;
+  minutos_seg = 60 * minutos;
+  total_seg = horas_seg + minutos_seg + seg;
 
-console.log("Los segundos equivalentes son: " + total_seg);
+  console.log("Los segundos equivalentes son: " + total_seg);
+}
+
+calcularSegundosEquivalentes();
