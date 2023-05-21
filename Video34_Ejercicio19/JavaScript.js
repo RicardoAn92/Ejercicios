3bfa05daf198b30175b4118c67640171d28fdb22
RicardoAn92 <127223869+RicardoAn92@users.noreply.github.com>
2023-05-20 23:06:13 -0500

    JavaScript.js

--- Video34_Ejercicio19/JavaScript.js
+++ Video34_Ejercicio19/JavaScript.js
@@ -1,41 +1,36 @@
 const leer = require("prompt-sync")();
 
-let kilos, descuento, descuento_final, precioK, precioI;
+function calcularPrecioManzanas() {
+  let kilos, descuento, descuento_final, precioK, precioI;
 
-console.log("Cuánto cuesta el kilo de manzanas?");
-precioK = parseFloat(leer());
+  console.log("Cuánto cuesta el kilo de manzanas?");
+  precioK = parseFloat(leer());
 
-console.log("Cuántos kilos ha comprado?");
-kilos = parseFloat(leer());
+  console.log("Cuántos kilos ha comprado?");
+  kilos = parseFloat(leer());
 
-precioI = precioK * kilos;
+  precioI = precioK * kilos;
 
-if (kilos >= 0 && kilos <= 2) {
-   console.log("Usted pagará: $", precioI);
-} else {
-   if (kilos > 2 && kilos <= 5) {
-      descuento = precioI * 0.1;
-      descuento_final = precioI - descuento;
-      console.log("Su precio sin descuento es: $", precioI);
-      console.log("Su precio con descuento del 10% es: $", descuento_final);
-   } else {
-      if (kilos > 5 && kilos <= 10) {
-         descuento = precioI * 0.15;
-         descuento_final = precioI - descuento;
-         console.log("Su precio sin descuento es: $", precioI);
-         console.log("Su precio con descuento del 15% es: $", descuento_final);
-      } else {
-         if (kilos > 10 && kilos <= 100) {
-            descuento = precioI * 0.2;
-            descuento_final = precioI - descuento;
-            console.log("Su precio sin descuento es: $", precioI);
-            console.log(
-               "Su precio con descuento del 20% es: $",
-               descuento_final
-            );
-         } else {
-            console.log("Kilos incorrectos");
-         }
-      }
-   }
+  if (kilos >= 0 && kilos <= 2) {
+    console.log("Usted pagará: $", precioI);
+  } else if (kilos > 2 && kilos <= 5) {
+    descuento = precioI * 0.1;
+    descuento_final = precioI - descuento;
+    console.log("Su precio sin descuento es: $", precioI);
+    console.log("Su precio con descuento del 10% es: $", descuento_final);
+  } else if (kilos > 5 && kilos <= 10) {
+    descuento = precioI * 0.15;
+    descuento_final = precioI - descuento;
+    console.log("Su precio sin descuento es: $", precioI);
+    console.log("Su precio con descuento del 15% es: $", descuento_final);
+  } else if (kilos > 10 && kilos <= 100) {
+    descuento = precioI * 0.2;
+    descuento_final = precioI - descuento;
+    console.log("Su precio sin descuento es: $", precioI);
+    console.log("Su precio con descuento del 20% es: $", descuento_final);
+  } else {
+    console.log("Kilos incorrectos");
+  }
 }
+
+calcularPrecioManzanas();
