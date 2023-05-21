bd2121f3d532f75594772b443a96173f88e1ea35
RicardoAn92 <127223869+RicardoAn92@users.noreply.github.com>
2023-05-20 22:44:41 -0500

    JavaScript.js

--- Video29_Ejercicio14/JavaScript.js
+++ Video29_Ejercicio14/JavaScript.js
@@ -1,12 +1,16 @@
 const leer = require("prompt-sync")();
 
-let num;
+function verificarParidad() {
+  let num;
 
-console.log("Ingrese número: ");
-num = parseInt(leer());
+  console.log("Ingrese número: ");
+  num = parseInt(leer());
 
-if (num % 2 === 0) {
-   console.log("Par");
-} else {
-   console.log("Impar");
+  if (num % 2 === 0) {
+    console.log("Par");
+  } else {
+    console.log("Impar");
+  }
 }
+
+verificarParidad();
