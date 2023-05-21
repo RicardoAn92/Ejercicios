7a7ec2f5993b56623b3d1b8fb6133a82603651f0
RicardoAn92 <127223869+RicardoAn92@users.noreply.github.com>
2023-05-20 23:03:33 -0500

    JavaScript.js

--- Video33_Ejercicio18/JavaScript.js
+++ Video33_Ejercicio18/JavaScript.js
@@ -1,26 +1,31 @@
 const leer = require("prompt-sync")();
 
-let num1, num2, num3;
+function encontrarNumeroMayor() {
+  let num1, num2, num3;
 
-console.log("Ingrese número 1: ");
-num1 = parseInt(leer());
+  console.log("Ingrese número 1: ");
+  num1 = parseInt(leer());
 
-console.log("Ingrese número 2: ");
-num2 = parseInt(leer());
+  console.log("Ingrese número 2: ");
+  num2 = parseInt(leer());
 
-console.log("Ingrese número 3: ");
-num3 = parseInt(leer());
+  console.log("Ingrese número 3: ");
+  num3 = parseInt(leer());
 
-if (num1 > num2 && num1 > num3) {
-   console.log("El número 1 es mayor");
-} else {
-   if (num2 > num1 && num2 > num3) {
+  if (num1 > num2 && num1 > num3) {
+    console.log("El número 1 es mayor");
+  } else {
+    if (num2 > num1 && num2 > num3) {
       console.log("El número 2 es mayor");
-   } else {
+    } else {
       if (num3 > num1 && num3 > num2) {
-         console.log("El número 3 es mayor");
+        console.log("El número 3 es mayor");
       } else {
-         console.log("Son iguales");
+        console.log("Son iguales");
       }
-   }
+    }
+  }
 }
+
+
+encontrarNumeroMayor();
