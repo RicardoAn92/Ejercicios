dcb229fc09c6c2d5f8a84ece36f1e8340d12268f
RicardoAn92 <127223869+RicardoAn92@users.noreply.github.com>
2023-05-20 23:09:24 -0500

    JavaScript.js

--- Video35_Ejercicio20/JavaScript.js
+++ Video35_Ejercicio20/JavaScript.js
@@ -1,34 +1,39 @@
 const leer = require("prompt-sync")();
-let num;
 
-do {
-   console.log("Digite un número del día de la semana (1-7)");
-   num = parseInt(leer());
+function obtenerDiaSemana() {
+  let num;
 
-   switch (num) {
+  do {
+    console.log("Digite un número del día de la semana (1-7)");
+    num = parseInt(leer());
+
+    switch (num) {
       case 1:
-         console.log("Lunes");
-         break;
+        console.log("Lunes");
+        break;
       case 2:
-         console.log("Martes");
-         break;
+        console.log("Martes");
+        break;
       case 3:
-         console.log("Miércoles");
-         break;
+        console.log("Miércoles");
+        break;
       case 4:
-         console.log("Jueves");
-         break;
+        console.log("Jueves");
+        break;
       case 5:
-         console.log("Viernes");
-         break;
+        console.log("Viernes");
+        break;
       case 6:
-         console.log("Sábado");
-         break;
+        console.log("Sábado");
+        break;
       case 7:
-         console.log("Domingo");
-         break;
+        console.log("Domingo");
+        break;
       default:
-         console.log("Error, no existe día para ese número");
-         break;
-   }
-} while (!(num >= 1 && num <= 7));
+        console.log("Error, no existe día para ese número");
+        break;
+    }
+  } while (!(num >= 1 && num <= 7));
+}
+
+obtenerDiaSemana();
