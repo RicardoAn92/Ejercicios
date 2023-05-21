06b7e053e44fbaf9215b483d7f8691edde26194c
RicardoAn92 <127223869+RicardoAn92@users.noreply.github.com>
2023-05-20 22:27:22 -0500

    JavaScript.js

--- Video24_Ejercicio10/JavaScript.js
+++ Video24_Ejercicio10/JavaScript.js
@@ -1,29 +1,35 @@
+const prompt = require("prompt-sync")();
+
 const redondeo = (numero) => Math.round(numero * 100) / 100;
 
-let hombres, mujeres, grupo;
-let porcen_hombres, porcen_mujeres;
+function calcularPorcentajeHombresYMujeres() {
+  let hombres, mujeres, grupo;
+  let porcen_hombres, porcen_mujeres;
 
-console.log("Ingrese la cantidad de hombres: ");
-hombres = parseInt(prompt());
+  console.log("Ingrese la cantidad de hombres: ");
+  hombres = parseInt(prompt());
 
-console.log("Ingrese la cantidad de mujeres: ");
-mujeres = parseInt(prompt());
+  console.log("Ingrese la cantidad de mujeres: ");
+  mujeres = parseInt(prompt());
 
-grupo = hombres + mujeres;
-porcen_hombres = (hombres / grupo) * 100;
-porcen_mujeres = (mujeres / grupo) * 100;
+  grupo = hombres + mujeres;
+  porcen_hombres = (hombres / grupo) * 100;
+  porcen_mujeres = (mujeres / grupo) * 100;
 
-console.log(
-   "El porcentaje de hombres en un grupo de " +
+  console.log(
+    "El porcentaje de hombres en un grupo de " +
       grupo +
       " estudiantes es: " +
       redondeo(porcen_hombres) +
       "%"
-);
-console.log(
-   "El porcentaje de mujeres en un grupo de " +
+  );
+  console.log(
+    "El porcentaje de mujeres en un grupo de " +
       grupo +
       " estudiantes es: " +
       redondeo(porcen_mujeres) +
       "%"
-);
+  );
+}
+
+calcularPorcentajeHombresYMujeres();
