300025a3ff400193dc82b946d352b405ef01b4ee
RicardoAn92 <127223869+RicardoAn92@users.noreply.github.com>
2023-05-20 22:09:35 -0500

    JavaScript.js

--- Video15_Ejercicio1/JavaScript.js
+++ Video15_Ejercicio1/JavaScript.js
@@ -1,29 +1,33 @@
 const prompt = require("prompt-sync")();
 
-let valorAbsoluto,
-   valorTruncado,
-   valorRedondeado,
-   raizCuadrada,
-   seno,
-   logaNatural,
-   funExp,
-   numAleatorio;
+function calcularOperaciones() {
+  let valorAbsoluto,
+    valorTruncado,
+    valorRedondeado,
+    raizCuadrada,
+    seno,
+    logaNatural,
+    funExp,
+    numAleatorio;
 
-const num = parseFloat(prompt("Digite un numero: "));
-valorAbsoluto = Math.abs(num);
-valorTruncado = Math.trunc(num);
-valorRedondeado = Math.round(num);
-raizCuadrada = Math.sqrt(num);
-seno = Math.sin(num);
-logaNatural = Math.log(num);
-funExp = Math.exp(num);
-numAleatorio = Math.random();
+  const num = parseFloat(prompt("Digite un numero: "));
+  valorAbsoluto = Math.abs(num);
+  valorTruncado = Math.trunc(num);
+  valorRedondeado = Math.round(num);
+  raizCuadrada = Math.sqrt(num);
+  seno = Math.sin(num);
+  logaNatural = Math.log(num);
+  funExp = Math.exp(num);
+  numAleatorio = Math.random();
 
-console.log("El valor absoluto es:", valorAbsoluto);
-console.log("El valor truncado es:", valorTruncado);
-console.log("El valor redondeado es:", valorRedondeado);
-console.log("La raíz cuadrada es:", raizCuadrada);
-console.log("El seno es:", seno);
-console.log("El logaritmo natural es:", logaNatural);
-console.log("La función exponencial es:", funExp);
-console.log("El número aleatorio es:", numAleatorio);
+  console.log("El valor absoluto es:", valorAbsoluto);
+  console.log("El valor truncado es:", valorTruncado);
+  console.log("El valor redondeado es:", valorRedondeado);
+  console.log("La raíz cuadrada es:", raizCuadrada);
+  console.log("El seno es:", seno);
+  console.log("El logaritmo natural es:", logaNatural);
+  console.log("La función exponencial es:", funExp);
+  console.log("El número aleatorio es:", numAleatorio);
+}
+
+calcularOperaciones();
