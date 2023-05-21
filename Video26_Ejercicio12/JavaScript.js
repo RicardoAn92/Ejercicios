4a4dbc0021de4739ec90b2360d416ff5f507f947
RicardoAn92 <127223869+RicardoAn92@users.noreply.github.com>
2023-05-20 22:33:18 -0500

    JavaScript.js

--- Video26_Ejercicio12/JavaScript.js
+++ Video26_Ejercicio12/JavaScript.js
@@ -1,11 +1,15 @@
-const leer = require("prompt-sync")();
+const calcularPrecioFinal = () => {
+  const leer = require("prompt-sync")();
 
-let precio, descuento, precio_final;
+  let precio, descuento, precio_final;
 
-console.log("Digite el precio a pagar: ");
-precio = parseFloat(leer());
+  console.log("Digite el precio a pagar: ");
+  precio = parseFloat(leer());
 
-descuento = precio * 0.15;
-precio_final = precio - descuento;
+  descuento = precio * 0.15;
+  precio_final = precio - descuento;
 
-console.log("El precio a pagar es: " + precio_final);
+  console.log("El precio a pagar es: " + precio_final);
+}
+
+calcularPrecioFinal();
