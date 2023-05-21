e0c4816db23d9f0f6fc3db8bf4e0dbf3d1e3b2ea
RicardoAn92 <127223869+RicardoAn92@users.noreply.github.com>
2023-05-20 22:14:52 -0500

    JavaScript.js

--- Video17_Ejercicio3/JavaScript.js
+++ Video17_Ejercicio3/JavaScript.js
@@ -1,11 +1,15 @@
 const leer = require("prompt-sync")();
 
-let a, b;
-let result;
+function calcularResultado() {
+  let a, b;
+  let result;
 
-a = parseFloat(leer("Digite el valor de a: "));
-b = parseFloat(leer("Digite el valor de b: "));
+  a = parseFloat(leer("Digite el valor de a: "));
+  b = parseFloat(leer("Digite el valor de b: "));
 
-result = (3 + 5 * 8 < 3 && (-6 / 3) * 4 + 2 < 2) || a > b;
+  result = (3 + 5 * 8 < 3 && (-6 / 3) * 4 + 2 < 2) || a > b;
 
-console.log("El resultado es: " + result);
+  console.log("El resultado es: " + result);
+}
+
+calcularResultado();
