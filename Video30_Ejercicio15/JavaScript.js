2c5dcd1a3e4b06a01e529fcdbb495aed88c0e71c
RicardoAn92 <127223869+RicardoAn92@users.noreply.github.com>
2023-05-20 22:51:59 -0500

    JavaScript.js

--- Video30_Ejercicio15/JavaScript.js
+++ Video30_Ejercicio15/JavaScript.js
@@ -1,16 +1,20 @@
 const leer = require("prompt-sync")();
 
-let califi1, califi2, califi3, promedio;
+function calcularPromedioYVerificarAprobacion() {
+  let califi1, califi2, califi3, promedio;
 
-console.log("Ingrese las 3 calificaciones: ");
-califi1 = parseFloat(leer());
-califi2 = parseFloat(leer());
-califi3 = parseFloat(leer());
+  console.log("Ingrese las 3 calificaciones:");
+  califi1 = parseFloat(leer());
+  califi2 = parseFloat(leer());
+  califi3 = parseFloat(leer());
 
-promedio = Math.trunc((califi1 + califi2 + califi3) / 3);
+  promedio = Math.trunc((califi1 + califi2 + califi3) / 3);
 
-if (promedio >= 70) {
-   console.log("Aprobado con promedio de: " + promedio);
-} else {
-   console.log("Desaprobado con promedio de: " + promedio);
+  if (promedio >= 70) {
+    console.log("Aprobado con promedio de: " + promedio);
+  } else {
+    console.log("Desaprobado con promedio de: " + promedio);
+  }
 }
+
+calcularPromedioYVerificarAprobacion();
