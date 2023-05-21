c4e6f822179f2bbca5b11c6c980b19965c603f8a
RicardoAn92 <127223869+RicardoAn92@users.noreply.github.com>
2023-05-20 22:14:12 -0500

    JavaScript.js

--- Video16_Ejercicio2/JavaScript.js
+++ Video16_Ejercicio2/JavaScript.js
@@ -1,11 +1,16 @@
 const prompt = require("prompt-sync")();
 
-let resultado;
+function video16Ejercicio2() {
+  let resultado;
 
-const a = parseFloat(prompt("Ingrese a:"));
-const b = parseFloat(prompt("Ingrese b:"));
-const c = parseFloat(prompt("Ingrese c:"));
+  const a = parseFloat(prompt("Ingrese a:"));
+  const b = parseFloat(prompt("Ingrese b:"));
+  const c = parseFloat(prompt("Ingrese c:"));
 
-resultado = (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+  resultado = (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
 
-console.log("El resultado es: ", resultado);
+  console.log("El resultado es: ", resultado);
+}
+
+video16Ejercicio2();
+
