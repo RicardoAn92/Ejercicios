8e8a6a46a9607eb2973ae94751afb54b2d8cf199
RicardoAn92 <127223869+RicardoAn92@users.noreply.github.com>
2023-05-20 23:11:25 -0500

    JavaScript.js

--- Video36_Ejercicio21/JavaScript.js
+++ Video36_Ejercicio21/JavaScript.js
@@ -1,31 +1,36 @@
 const leer = require("prompt-sync")();
-let decada;
 
-do {
-   console.log("Digite una década en el rango 10-60");
-   decada = parseInt(leer());
+function obtenerBodas() {
+  let decada;
 
-   switch (decada) {
+  do {
+    console.log("Digite una década en el rango 10-60");
+    decada = parseInt(leer());
+
+    switch (decada) {
       case 10:
-         console.log("Bodas de Hojalata");
-         break;
+        console.log("Bodas de Hojalata");
+        break;
       case 20:
-         console.log("Bodas de Porcelana");
-         break;
+        console.log("Bodas de Porcelana");
+        break;
       case 30:
-         console.log("Bodas de Perlas");
-         break;
+        console.log("Bodas de Perlas");
+        break;
       case 40:
-         console.log("Bodas de Rubí");
-         break;
+        console.log("Bodas de Rubí");
+        break;
       case 50:
-         console.log("Bodas de Oro");
-         break;
+        console.log("Bodas de Oro");
+        break;
       case 60:
-         console.log("Bodas de Diamante");
-         break;
+        console.log("Bodas de Diamante");
+        break;
       default:
-         console.log("Rango incorrecto, ingrese de nuevo");
-         break;
-   }
-} while (!(decada >= 10 && decada <= 60));
+        console.log("Rango incorrecto, ingrese de nuevo");
+        break;
+    }
+  } while (!(decada >= 10 && decada <= 60));
+}
+
+obtenerBodas();
